c5e10c80ae5f9df728ea06435ea98d9add14db02
Milanninio9 <97458055+Milanninio9@users.noreply.github.com>
2026-09-07 11:53:01 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 
-
 import re                                         # Regex fuer die Hobby-Spalte
 import pandas as pd                               # Einlesen und Umformen der Quelldaten
 from pathlib import Path                          # plattformunabhaengige Pfade
